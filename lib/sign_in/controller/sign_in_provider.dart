e04ab563787edaeca18e457fd3237cab37afafda
NIKHIL K <129831806+nikhilmkda@users.noreply.github.com>
2024-06-21 22:09:47 +0530

    added new sha key for the project using

--- lib/sign_in/controller/sign_in_provider.dart
+++ lib/sign_in/controller/sign_in_provider.dart
@@ -1,25 +1,25 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignInProvider with ChangeNotifier {
+  Future<bool> signInWithGoogle() async {
+    try {
+      // Trigger Google Sign-in flow
+      final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithProvider(googleProvider);
 
-Future<bool> signInWithGoogle() async {
-  try {
-    // Trigger Google Sign-in flow
-    final GoogleAuthProvider googleProvider = GoogleAuthProvider();
-    final UserCredential userCredential = await FirebaseAuth.instance.signInWithProvider(googleProvider);
-
-    // User signed in successfully
-    print("Sign In Successful! User: ${userCredential.user?.email}");
-    return true;
-  } on FirebaseAuthException catch (error) {
-    print("Sign In Failed: ${error.code} - ${error.message}");
-    return false; // Or throw an exception for further handling
-  } catch (error) {
-    print("An unexpected error occurred: $error");
-    return false; // Or throw an exception for further handling
+      // User signed in successfully
+      log("Sign In Successful! User: ${userCredential.user?.email}");
+      return true;
+    } on FirebaseAuthException catch (error) {
+      log("Sign In Failed: ${error.code} - ${error.message}");
+      return false; // Or throw an exception for further handling
+    } catch (error) {
+      log("An unexpected error occurred: $error");
+      return false; // Or throw an exception for further handling
+    }
   }
-}
-
-  
 }
